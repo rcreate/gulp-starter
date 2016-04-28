973f798b4aaf632f056cfb8f58323e5c8a67e224
Ricardo Schmidt <ricardo.schmidt84@gmail.com>
2016-04-28 23:33:47 +0200

    Fix getConfig for correct path

--- gulpfile.js/lib/getConfig.js
+++ gulpfile.js/lib/getConfig.js
@@ -2,6 +2,6 @@ var path = require('path')
 var fs = require('fs')
 
 module.exports = function () {
-  var asSubmodule = fs.existsSync(path.resolve('../../../../node_modules/gulp-starter'));
+  var asSubmodule = fs.existsSync(path.resolve('../../node_modules/gulp-starter'))
   return require(asSubmodule?"../../../../config":"../config")
 }
