6d874dec6c72b02388caa895b25c37eee660e5c7
Ricardo Schmidt <r.schmidt@queo-group.com>
2016-05-03 12:38:50 +0200

    Fix styleguide task for applying custom styles

--- gulpfile.js/tasks/styleguide.js
+++ gulpfile.js/tasks/styleguide.js
@@ -1,17 +1,23 @@
 var config       = require('../lib/getConfig')()
 if(!config.tasks.styleguide) return
 
-var path = require('path')
-var gulp = require('gulp')
-var watch = require('gulp-watch')
-var sass = require('gulp-sass');
-var styleguide = require('sc5-styleguide');
-var gulpSequence = require('gulp-sequence')
+var path = require('path'),
+    gulp = require('gulp'),
+    del = require('del'),
+    watch = require('gulp-watch'),
+    sass = require('gulp-sass'),
+    styleguide = require('sc5-styleguide'),
+    gulpSequence = require('gulp-sequence'),
+    rename = require('gulp-rename');
 
 var paths = {
   src: path.join(config.root.src, config.tasks.styleguide.src, '/**/*.{' + config.tasks.styleguide.extensions + '}'),
-  entry: path.resolve(config.root.src, config.tasks.styleguide.entry),
+  entry: path.join(config.root.src, config.tasks.styleguide.src, config.tasks.styleguide.entry),
   dest: path.join(config.root.dest, config.tasks.styleguide.dest)
+}
+
+var sgCleanTask = function () {
+    del(paths.dest, {force: true});
 }
 
 var sgGenerateTask = function () {
@@ -22,16 +28,19 @@ var sgGenerateTask = function () {
       port: 4000,
       rootPath: paths.dest,
       enableJade: (config.tasks.jade?true:false),
-      overviewPath: path.resolve(config.root.src, config.tasks.styleguide.overview)
+      overviewPath: path.join(config.root.src, config.tasks.styleguide.overview),
+      styleVariables: path.join(config.root.src, config.tasks.styleguide.src, 'config/_config.sass'),
+      parsers: {
+          sass: 'sass'
+      },
     }))
     .pipe(gulp.dest(paths.dest));
 }
 
 var sgApplyStylesTask = function () {
   return gulp.src(paths.entry)
-    .pipe(sass({
-      errLogToConsole: true
-    }))
+    .pipe(sass())
+    .pipe(rename('styleguide.css'))
     .pipe(styleguide.applyStyles())
     .pipe(gulp.dest(paths.dest));
 }
@@ -46,9 +55,10 @@ var styleguideLaunchTask = function(cb) {
 }
 
 var styleguideTask = function(cb) {
-    gulpSequence(['styleguide:generate', 'styleguide:applystyles'], 'styleguide:watch', 'styleguide:launch', cb);
+    gulpSequence('styleguide:clean', 'styleguide:generate', 'styleguide:applystyles', 'styleguide:watch', 'styleguide:launch', cb);
 }
 
+gulp.task('styleguide:clean', sgCleanTask)
 gulp.task('styleguide:generate', sgGenerateTask)
 gulp.task('styleguide:applystyles', sgApplyStylesTask)
 gulp.task('styleguide:launch', styleguideLaunchTask)
