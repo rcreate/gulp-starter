813b60f7252a667a5906a1565f909f961ff8b75d
Ricardo Schmidt <r.schmidt@queo-group.com>
2016-06-15 16:59:24 +0200

    Add source-map for production release, add environment to server task for standalone running

--- extras/tasks/server.js
+++ extras/tasks/server.js
@@ -7,17 +7,16 @@ var logger   = require('morgan')
 var open     = require('open')
 var path     = require('path')
 
-var settings = {
-  root: path.resolve(process.env.PWD, dest()),
-  port: process.env.PORT || 5000,
-  logLevel: process.env.NODE_ENV ? 'combined' : 'dev',
-  staticOptions: {
-    extensions: ['html'],
-    maxAge: '31556926'
-  }
-}
-
 var serverTask = function() {
+  var settings = {
+    root: path.resolve(process.env.PWD, dest()),
+    port: process.env.PORT || 5000,
+    logLevel: process.env.NODE_ENV ? 'combined' : 'dev',
+    staticOptions: {
+      extensions: ['html'],
+      maxAge: '31556926'
+    }
+  }
   var url = 'http://localhost:' + settings.port
 
   express()
@@ -30,5 +29,13 @@ var serverTask = function() {
   open(url)
 }
 
-gulp.task('server', serverTask)
+var runServerTask = function (env) {
+  global.environment = env
+  serverTask()
+}
+
+gulp.task('server', function() { runServerTask('development') })
+gulp.task('server:development', function() { runServerTask('development') })
+gulp.task('server:production', function() { runServerTask('production') })
+gulp.task('server:distribution', function() { runServerTask('distribution') })
 module.exports = serverTask
