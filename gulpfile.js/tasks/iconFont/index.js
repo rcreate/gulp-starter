66743386c47f68d7f9c048f7c6b115ded1dcfd1e
Ricardo Schmidt <r.schmidt@queo-group.com>
2016-06-15 17:09:28 +0200

    Fix iconFont task in production mode

--- gulpfile.js/tasks/iconFont/index.js
+++ gulpfile.js/tasks/iconFont/index.js
@@ -10,27 +10,28 @@ var package          = require('../../lib/getPackage')()
 var path             = require('path')
 var url              = require('url')
 
-var fontDest = dest(config.tasks.iconFont.dest)
-var cssDest = dest(config.tasks.css.dest)
-
-var settings = {
-  name: package.name + ' icons',
-  src: path.join(config.root.src, config.tasks.iconFont.src, '/*.svg'),
-  dest: fontDest,
-  sassDest: path.join(config.root.src, config.tasks.css.src, config.tasks.iconFont.sassDest),
-  template: path.normalize('./gulpfile.js/tasks/iconFont/template.sass'),
-  sassOutputName: '_icons.sass',
-  fontPath: url.resolve('.',path.relative(cssDest, fontDest)),
-  className: 'icon',
-  options: {
-    timestamp: 0, // see https://github.com/fontello/svg2ttf/issues/33
-    fontName: 'icons',
-    normalize: false,
-    formats: config.tasks.iconFont.extensions
-  }
-}
-
 var iconFontTask = function() {
+  var fontDest = dest(config.tasks.iconFont.dest)
+  var cssDest = dest(config.tasks.css.dest)
+
+  var settings = {
+    name: package.name + ' icons',
+    src: path.join(config.root.src, config.tasks.iconFont.src, '/*.svg'),
+    dest: fontDest,
+    sassDest: path.join(config.root.src, config.tasks.css.src, config.tasks.iconFont.sassDest),
+    template: path.normalize('./gulpfile.js/tasks/iconFont/template.sass'),
+    sassOutputName: '_icons.sass',
+    fontPath: url.resolve('.',path.relative(cssDest, fontDest)),
+    className: 'icon',
+    options: {
+      timestamp: 0, // see https://github.com/fontello/svg2ttf/issues/33
+      fontName: 'icons',
+      normalize: false,
+      formats: config.tasks.iconFont.extensions
+    }
+  }
+
+  console.log(settings.dest)
   return gulp.src(settings.src)
     .pipe(iconfont(settings.options))
     .on('glyphs', generateIconSass(settings))
