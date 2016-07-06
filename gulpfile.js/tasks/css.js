1613379d1b1637b598420507384509febe22ec61
Ricardo Schmidt <r.schmidt@queo-group.com>
2016-07-06 16:43:44 +0200

    Add less support

--- gulpfile.js/tasks/css.js
+++ gulpfile.js/tasks/css.js
@@ -4,13 +4,19 @@ if(!config.tasks.css) return
 var gulp         = require('gulp')
 var gulpif       = require('gulp-if')
 var browserSync  = require('browser-sync')
-var sass         = require('gulp-sass')
 var sourcemaps   = require('gulp-sourcemaps')
 var handleErrors = require('../lib/handleErrors')
 var dest         = require('../lib/dest')
 var autoprefixer = require('gulp-autoprefixer')
 var path         = require('path')
 var cssnano      = require('gulp-cssnano')
+
+// decide which plugin should be used (sass or less)
+if( typeof config.tasks.css.type === "undefined" ){
+  config.tasks.css['type'] = 'sass'
+}
+var pluginType = config.tasks.css.type
+var plugin = require('gulp-'+pluginType)
 
 var exclude = path.join(config.root.src, config.tasks.css.src, '**/{' + config.tasks.css.excludeFolders.join(',') + '}/**/*.{' + config.tasks.css.extensions + '}')
 var paths = {
@@ -23,7 +29,7 @@ var paths = {
 var cssTask = function () {
   return gulp.src(paths.src)
     .pipe(gulpif(!global.production, sourcemaps.init()))
-    .pipe(sass(config.tasks.css.sass))
+    .pipe(plugin(config.tasks.css[pluginType]))
     .on('error', handleErrors)
     .pipe(autoprefixer(config.tasks.css.autoprefixer))
     .pipe(gulpif(global.environment === 'development', sourcemaps.write()))
