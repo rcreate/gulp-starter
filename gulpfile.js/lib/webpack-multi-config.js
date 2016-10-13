c1722c6ce589fc76e56bff90fbb1038cb2d2a1bb
Ricardo Schmidt <r.schmidt@queo-group.com>
2016-10-13 14:52:34 +0200

    Add package.json as data to pug compiler

--- gulpfile.js/lib/webpack-multi-config.js
+++ gulpfile.js/lib/webpack-multi-config.js
@@ -11,7 +11,6 @@ module.exports = function(env) {
   var jsSrc = path.resolve(process.env.PWD, GULP_CONFIG.root.src, GULP_CONFIG.tasks.js.src)
   var jsDest = dest(GULP_CONFIG.tasks.js.dest)
   var publicPath = pathToUrl(GULP_CONFIG.tasks.js.dest, '/')
-
   var extensions = GULP_CONFIG.tasks.js.extensions.map(function(extension) {
     return '.' + extension
   })
