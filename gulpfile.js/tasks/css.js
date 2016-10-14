f1fba646cb5ad25e065bc16ccda7b8bb5d4d6c5f
Ricardo Schmidt <r.schmidt@queo-group.com>
2016-10-14 18:12:21 +0200

    Add support for shorthand npm module imports in sass files

--- gulpfile.js/tasks/css.js
+++ gulpfile.js/tasks/css.js
@@ -7,10 +7,12 @@ var sourcemaps = require('gulp-sourcemaps')
 var handleErrors = require('../lib/handleErrors')
 var dest = require('../lib/dest')
 var globExtension = require('../lib/globExtension')
+var importer = require('npm-sass-require')
 var autoprefixer = require('gulp-autoprefixer')
 var path = require('path')
 var cssnano = require('gulp-cssnano')
 var rename = require('gulp-rename')
+var extend = require('extend')
 
 var cssTask = function () {
 
@@ -21,6 +23,7 @@ var cssTask = function () {
   var pluginType = GULP_CONFIG.tasks.css.type
   var plugin = require('gulp-'+pluginType)
   var extensions = globExtension(GULP_CONFIG.tasks.css.extensions)
+  var options = extend(GULP_CONFIG.tasks.css[pluginType], {importer: importer})
 
   var deployUncompressed = (global.environment === 'distribution' && GULP_CONFIG.tasks.css.deployUncompressed)
 
@@ -34,7 +37,7 @@ var cssTask = function () {
 
   return gulp.src(paths.src)
     .pipe(gulpif(!global.production, sourcemaps.init()))
-    .pipe(plugin(GULP_CONFIG.tasks.css[pluginType]))
+    .pipe(plugin(options))
     .on('error', handleErrors)
     .pipe(autoprefixer(GULP_CONFIG.tasks.css.autoprefixer))
     .pipe(gulpif(deployUncompressed, gulp.dest(dest(GULP_CONFIG.tasks.css.dest))))
