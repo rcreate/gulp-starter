3ab2c05cda796db892034926f434dbaa244a3006
Ricardo Schmidt <r.schmidt@queo-group.com>
2016-11-07 17:18:21 +0100

    Support wildcard image extensions

--- gulpfile.js/tasks/images.js
+++ gulpfile.js/tasks/images.js
@@ -8,24 +8,25 @@ var imagemin    = require('gulp-imagemin')
 var path        = require('path')
 var gulpif       = require('gulp-if')
 var dest        = require('../lib/dest')
+var globExt     = require('../lib/globExtension')
 
 var paths = {
-  src: path.join(config.root.src, config.tasks.images.src, '/**/*.{' + config.tasks.images.extensions + '}'),
+    src: path.join(config.root.src, config.tasks.images.src, '/**/*.' + globExt(config.tasks.images.extensions)),
 }
 
 var imagesTask = function() {
-  return gulp.src([paths.src, , '*!README.md'])
+    return gulp.src([paths.src, '*!README.md'])
     // Ignore unchanged files
-    .pipe(gulpif(global.environment === 'development', changed(dest(config.tasks.images.dest))))
-    .pipe(gulpif(global.environment !== 'development', imagemin())) // Optimize
-    .pipe(gulp.dest(dest(config.tasks.images.dest)))
+        .pipe(gulpif(global.environment === 'development', changed(dest(config.tasks.images.dest))))
+        .pipe(gulpif(global.environment !== 'development', imagemin())) // Optimize
+        .pipe(gulp.dest(dest(config.tasks.images.dest)))
 
-  .pipe(browserSync.stream())
+        .pipe(browserSync.stream())
 }
 
 var devTask = function () {
-  global.environment = 'development'
-  imagesTask()
+    global.environment = 'development'
+    imagesTask()
 }
 
 gulp.task('images', imagesTask)
