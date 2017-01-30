1d1da00fab1537e5420d7780db9f173c49076a1e
Ricardo Schmidt <r.schmidt@queo-group.com>
2017-01-30 16:17:33 +0100

    Fix glob rule for "excludeFolders" in stylesheet task

--- gulpfile.js/tasks/stylesheets.js
+++ gulpfile.js/tasks/stylesheets.js
@@ -22,26 +22,26 @@ var stylesheetsTask = function () {
   var plugin = require('gulp-'+pluginType)
   var extensions = globExt(TASK_CONFIG.stylesheets.extensions)
   var options = TASK_CONFIG.stylesheets[pluginType]
-
   var deployUncompressed = (global.environment === 'distribution' && TASK_CONFIG.stylesheets.deployUncompressed)
 
-  var exclude = '';
-  if( TASK_CONFIG.stylesheets.excludeFolders ) {
-      exclude = "!" + path.resolve(process.env.PWD, PATH_CONFIG.src, PATH_CONFIG.stylesheets.src, '**/{' + TASK_CONFIG.stylesheets.excludeFolders.join(',') + '}/**/*.' + extensions)
-  }
-
-  var paths = {
-    src: [
-        path.resolve(process.env.PWD, PATH_CONFIG.src, PATH_CONFIG.stylesheets.src, '**/*.' + extensions),
-        exclude
-    ],
-    dest: dest(PATH_CONFIG.stylesheets.dest)
-  }
-
+  // Additional include paths for sass processor
   if(TASK_CONFIG.stylesheets.sass && TASK_CONFIG.stylesheets.sass.includePaths) {
     options.includePaths = options.includePaths.map(function(includePath) {
-      return path.resolve(process.env.PWD, includePath)
-    })
+        return path.resolve(process.env.PWD, includePath)
+    });
+  }
+
+  // Build array of source and destination paths
+  var paths = {
+    src: [path.resolve(process.env.PWD, PATH_CONFIG.src, PATH_CONFIG.stylesheets.src, '**/*.' + extensions)],
+    dest: dest(PATH_CONFIG.stylesheets.dest)
+  };
+
+  // Prevent following folders from processing
+  if( TASK_CONFIG.stylesheets.excludeFolders ) {
+      TASK_CONFIG.stylesheets.excludeFolders.forEach(function (excludePath) {
+          paths.src.push('!' + path.resolve(process.env.PWD, PATH_CONFIG.src, PATH_CONFIG.stylesheets.src, excludePath, '**/*.' + extensions))
+      });
   }
 
   return gulp.src(paths.src)
