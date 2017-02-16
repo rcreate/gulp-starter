bc835bdd046788e26e0cbd432390499f852e48df
Ricardo Schmidt <r.schmidt@queo-group.com>
2017-02-16 16:54:49 +0100

    Fix error if "plugins" was not defined in config (due to expected type "function")

--- gulpfile.js/lib/webpack-multi-config.js
+++ gulpfile.js/lib/webpack-multi-config.js
@@ -147,12 +147,15 @@ module.exports = function(env) {
     }
   }
 
-  //Add defined plugins and loaders for all environments
-  webpackConfig.plugins = webpackConfig.plugins.concat(TASK_CONFIG.javascripts.plugins(webpack) || [])
+  // Add defined plugins and loaders for all environments
+  if( TASK_CONFIG.javascripts.plugins ) {
+    webpackConfig.plugins = webpackConfig.plugins.concat(TASK_CONFIG.javascripts.plugins(webpack) || [])
+  }
   webpackConfig.module.loaders = webpackConfig.module.loaders.concat(TASK_CONFIG.javascripts.loaders || [])
 
-  //Additional loaders for development and production
   /**
+   * Additional loaders for development and production
+   *
    * @deprecated since version 4.1.0, define additional loaders in javascripts.development.loaders
    */
   if (TASK_CONFIG.javascripts[env+'Loaders']) {
@@ -161,7 +164,9 @@ module.exports = function(env) {
 
   // Additional plugins and loaders according to environment
   if (TASK_CONFIG.javascripts[env]) {
-    webpackConfig.plugins = webpackConfig.plugins.concat(TASK_CONFIG.javascripts[env].plugins(webpack) || [])
+    if( TASK_CONFIG.javascripts[env].plugins ) {
+      webpackConfig.plugins = webpackConfig.plugins.concat(TASK_CONFIG.javascripts[env].plugins(webpack) || [])
+    }
     webpackConfig.module.loaders = webpackConfig.module.loaders.concat(TASK_CONFIG.javascripts[env].loaders || [])
   }
 
